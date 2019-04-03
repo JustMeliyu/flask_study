613fb6a9dd7da05fafce7522efcae1dcec3275d8
JustMeliyu <642464509@qq.com>
2019-04-03 21:42:44 +0800

    quick_sort

--- training/quick_sort.py
+++ training/quick_sort.py
@@ -20,32 +20,20 @@ def quick_sort(origin_list):
 
 
 def quick_sort2(origin_list):
-    i = 0
-    j = len(origin_list) - 1
-    povit = origin_list[0]
-    while i < j:
-        while i < j and origin_list[j] >= povit:
-            j -= 1
-        if i < j:
-            origin_list[i] = origin_list[j]
-            i += 1
-
-        while i < j and origin_list[i] < povit:
-            i += 1
-
-
+    # a = [3, 40, 7, 34, 2, 89, 1, 432, 23, 89, 5, 2222]
     if len(origin_list) > 1:
         start_index = 1
         for i in range(len(origin_list) - 1, -1, -1):
             if start_index == i + 1:
                 # if origin_list[i] < origin_list[0]:
-                origin_list[0], origin_list[i] = origin_list[i], origin_list[0]
-                return quick_sort2(origin_list[:i]) + [origin_list[i]] + quick_sort2(origin_list[i + 1:])
+                origin_list[0], origin_list[start_index] = origin_list[start_index], origin_list[0]
+                return quick_sort2(origin_list[:start_index]) + [origin_list[start_index]] + quick_sort2(
+                    origin_list[start_index + 1:])
                 # else:
                 #     origin_list[0], origin_list[i - 1] = origin_list[i - 1], origin_list[0]
                 #     return quick_sort2(origin_list[:i - 1]) + [origin_list[i - 1]] + origin_list[i:]
             if origin_list[i] < origin_list[0]:
-                for j in range(start_index, len(origin_list)):
+                for j in range(start_index, i):
                     if origin_list[j] > origin_list[0]:
                         origin_list[j], origin_list[i] = origin_list[i], origin_list[j]
                         start_index = j + 1
@@ -54,15 +42,72 @@ def quick_sort2(origin_list):
         return origin_list
 
 
+def quick_sort3(myList, start, end):
+    # 判断low是否小于high,如果为false,直接返回
+    if start < end:
+        i, j = start, end
+        # 设置基准数
+        base = myList[i]
+
+        while i < j:
+            # 如果列表后边的数,比基准数大或相等,则前移一位直到有比基准数小的数出现
+            while (i < j) and (myList[j] >= base):
+                j = j - 1
+
+            # 如找到,则把第j个元素赋值给第个元素i,此时表中i,j个元素相等
+            myList[i] = myList[j]
+
+            # 同样的方式比较前半区
+            while (i < j) and (myList[i] <= base):
+                i = i + 1
+            myList[j] = myList[i]
+        # 做完第一轮比较之后,列表被分成了两个半区,并且i=j,需要将这个数设置回base
+        myList[i] = base
+
+        # 递归前后半区
+        quick_sort3(myList, start, i - 1)
+        quick_sort3(myList, j + 1, end)
+    return myList
+
+
+def quick_sort4(myList):
+    # 判断low是否小于high,如果为false,直接返回
+    if len(myList) > 0:
+        i = 0
+        j = len(myList) - 1
+        # 设置基准数
+        base = myList[i]
+
+        while i < j:
+            # 如果列表后边的数,比基准数大或相等,则前移一位直到有比基准数小的数出现
+            while (i < j) and (myList[j] >= base):
+                j = j - 1
+
+            # 如找到,则把第j个元素赋值给第个元素i,此时表中i,j个元素相等
+            myList[i] = myList[j]
+
+            # 同样的方式比较前半区
+            while (i < j) and (myList[i] <= base):
+                i = i + 1
+            myList[j] = myList[i]
+        # 做完第一轮比较之后,列表被分成了两个半区,并且i=j,需要将这个数设置回base
+        myList[i] = base
+
+        # 递归前后半区
+        return quick_sort4(myList[:i-1]) + [myList[i]] + quick_sort4(myList[j + 1:])
+    return myList
+
+
 if __name__ == "__main__":
     # a = [3, 40, 7, 34, 2, 89, 1, 432, 23, 89, 5, 2222]
     # a = [2, 89, 1, 432, 23, 89, 5, 2222]
     # print a[:3], [a[3]], a[4:]
     # a = [3, 1, 2, 34, 7, 89, 40, 432, 23, 89, 5, 2222]
-    # a = [3, 1, 2, 34, 5, 7, 89, 40, 432, 23, 89, 5, 2222]
-    a = [2, 1]
+    a = [5, 1, 2, 34, 5, 7, 89, 40, 432, 23, 89, 3, 2222]
+    # a = [2, 1, 3]
     # a = quick_sort(a)
-    a = quick_sort2(a)
+    # a = quick_sort2(a)
+    a = quick_sort4(a)
     print a
     # left:[2, 1]  mid:[3]  right:[40, 7, 34, 89, 432, 23, 89, 5, 2222]
     # left:[1] mid:[2] right:[]  mid:[3]  right:[40, 7, 34, 89, 432, 23, 89, 5, 2222]
