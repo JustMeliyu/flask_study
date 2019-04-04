1e170794ba5a8d72ea0c37e82f9a697126f9948a
JustMeliyu <642464509@qq.com>
2019-04-04 11:42:25 +0800

    finish quick sort

--- training/quick_sort.py
+++ training/quick_sort.py
@@ -19,27 +19,38 @@ def quick_sort(origin_list):
         return origin_list
 
 
-def quick_sort2(origin_list):
+def quick_sort2(myList):
     # a = [3, 40, 7, 34, 2, 89, 1, 432, 23, 89, 5, 2222]
-    if len(origin_list) > 1:
-        start_index = 1
-        for i in range(len(origin_list) - 1, -1, -1):
-            if start_index == i + 1:
-                # if origin_list[i] < origin_list[0]:
-                origin_list[0], origin_list[start_index] = origin_list[start_index], origin_list[0]
-                return quick_sort2(origin_list[:start_index]) + [origin_list[start_index]] + quick_sort2(
-                    origin_list[start_index + 1:])
-                # else:
-                #     origin_list[0], origin_list[i - 1] = origin_list[i - 1], origin_list[0]
-                #     return quick_sort2(origin_list[:i - 1]) + [origin_list[i - 1]] + origin_list[i:]
-            if origin_list[i] < origin_list[0]:
-                for j in range(start_index, i):
-                    if origin_list[j] > origin_list[0]:
-                        origin_list[j], origin_list[i] = origin_list[i], origin_list[j]
-                        start_index = j + 1
-                        break
-    else:
-        return origin_list
+    # 判断low是否小于high,如果为false,直接返回
+    if len(myList) > 0:
+        i = 1
+        j = len(myList) - 1
+        # 设置基准数
+        base = myList[0]
+
+        while i < j:
+            # 如果列表后边的数,比基准数大或相等,则前移一位直到有比基准数小的数出现
+            while (i < j) and (myList[j] >= base):
+                j = j - 1
+
+            # 同样的方式比较前半区
+            while (i < j) and (myList[i] <= base):
+                i = i + 1
+
+            # 如找到,则把第j个元素赋值给第个元素i,此时表中i,j个元素相等
+            if i < j:
+                myList[i], myList[j] = myList[j], myList[i]
+
+            # 同样的方式比较前半区
+            # while (i < j) and (myList[i] <= base):
+            #     i = i + 1
+            # myList[j] = myList[i]
+        # 做完第一轮比较之后,列表被分成了两个半区,并且i=j,需要将这个数设置回base
+        # myList[i] = base
+
+        # 递归前后半区
+        return quick_sort4(myList[:i]) + [myList[i]] + quick_sort4(myList[i + 1:])
+    return myList
 
 
 def quick_sort3(myList, start, end):
@@ -94,17 +105,17 @@ def quick_sort4(myList):
         myList[i] = base
 
         # 递归前后半区
-        return quick_sort4(myList[:i-1]) + [myList[i]] + quick_sort4(myList[j + 1:])
+        return quick_sort4(myList[:i]) + [myList[i]] + quick_sort4(myList[i + 1:])
     return myList
 
 
 if __name__ == "__main__":
-    # a = [3, 40, 7, 34, 2, 89, 1, 432, 23, 89, 5, 2222]
+    a = [3, 40, 7, 34, 2, 89, 1, 432, 23, 89, 5, 2222]
     # a = [2, 89, 1, 432, 23, 89, 5, 2222]
     # print a[:3], [a[3]], a[4:]
     # a = [3, 1, 2, 34, 7, 89, 40, 432, 23, 89, 5, 2222]
-    a = [5, 1, 2, 34, 5, 7, 89, 40, 432, 23, 89, 3, 2222]
-    # a = [2, 1, 3]
+    # a = [5, 1, 2, 34, 5, 7, 89, 40, 432, 23, 89, 3, 2222]
+    # a = [2, 1]
     # a = quick_sort(a)
     # a = quick_sort2(a)
     a = quick_sort4(a)
